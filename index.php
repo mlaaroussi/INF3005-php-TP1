165a845d254ef70ff2518b7278cedea35f39ddc4
Mohamed LAAROUSSI <laaroussi.mohamed@courrier.uqam.ca>
2015-03-13 00:52:57 -0400

    modification de css pour positionner les elements de la page index

--- index.php
+++ index.php
@@ -32,40 +32,42 @@
             </figure>
 
             <form id="formPrincipal">
-                <article id="choix-criteres">
-                    <h2>2.Adapter votre choix :</h2>                    
-                    <input type="hidden" id="cadre-choisi" name="cadre-choisi" >
-                    <h3>Dimensions :</h3>
-                    <label for="rangeH" >Hauteur: <span class="normal">(max. 100cm)</span> </label> 
-                    <input type="range" id="rangeH" value="50">
-                    <input type="text" name="hauteur" id="ht" value="50">
-                    <br/>
-                    <label for="rangeL" >Largeur: <span class="normal">(max. 100cm)</span> </label> 
-                    <input type="range" id="rangeL" value="50">
-                    <input type="text" name="largeur" id="lr" value="50">
-                    <br/>
-                    <label for="rangeP" >Profondeur: <span class="normal">(max. 5cm)</span> </label>
-                    <input type="range" id="rangeP" value="40">
-                    <input type="text" name="profondeur" id="pf" value="2">
-                    <br/>
-                    <label for="couleur" >Couleur: </label>
-                    <input type="color" name="couleur"/>
-                   
-                </article>
-                
-                <aside id="affichageRslt">
-                    <h2> Résultat de l'ecadrement <img id="chargement" src="img/chargement.gif" width="22" height="22" ></h2>                                    
-                    <img id="imgRslt" src="img/no-image.jpg" width="200" height="200" >                      
-                </aside>
+                <div id="corps">
+                    <div id="choixCriteres">
+                        <h2>2.Adapter votre choix :</h2>                    
+                        <input type="hidden" id="cadre-choisi" name="cadre-choisi" >
+                        <h3>Dimensions :</h3>
+                        <label for="rangeH" >Hauteur: <span class="normal">(max. 100cm)</span> </label> 
+                        <input type="range" id="rangeH" value="50">
+                        <input type="text" name="hauteur" id="ht" value="50">
+                        <br/>
+                        <label for="rangeL" >Largeur: <span class="normal">(max. 100cm)</span> </label> 
+                        <input type="range" id="rangeL" value="50">
+                        <input type="text" name="largeur" id="lr" value="50">
+                        <br/>
+                        <label for="rangeP" >Profondeur: <span class="normal">(max. 5cm)</span> </label>
+                        <input type="range" id="rangeP" value="40">
+                        <input type="text" name="profondeur" id="pf" value="2">
+                        <br/>
+                        <label for="couleur" >Couleur: </label>
+                        <input type="color" name="couleur"/>
 
-                <aside id="choix-fichier">
-                    <h2>3.Fournir un fichier contenant votre photo</h2>
-                    <input type="file" name="file" id="fichierImg" accept="image/*" required />              
-                    <br/>
-                    <input type="submit" value="Produire facture"/>
-                  
-                    <div id="message"></div>
-                </aside>                               
+
+                        <h2>3.Fournir un fichier contenant votre photo</h2>
+                        <input type="file" name="file" id="fichierImg" accept="image/*" required />              
+                        <br/>
+                        <input type="submit" value="Produire facture"/>
+
+                        <div id="message"></div>
+
+                    </div>
+
+                    <div id="affichageRslt">
+                        <h2> Résultat de l'ecadrement <img id="chargement" src="img/chargement.gif" width="22" height="22" ></h2>                                    
+                        <img id="imgRslt" src="img/no-image.jpg" width="150" height="150" >                      
+                    </div>
+                </div>
+
             </form>           
         </section>
 
